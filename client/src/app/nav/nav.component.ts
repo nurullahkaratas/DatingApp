bb3cf2a75e6993002a7b52b3a45daf40d7b2390e
nurullah.karatas <nurullah.karatas@acibadem.com>
2022-01-18 09:41:14 +0300

    nav-ts

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,30 +10,22 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
    public model:any={};
-  public loggedIn:boolean;
-  
+  currentUser$:Observable<User>;
   constructor(private accountService:AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    this.currentUser$=this.accountService.currentUser$;
   }
 
   login(){
     this.accountService.login(this.model).subscribe(response=>{
       console.log(response);
-      this.loggedIn=true;
+    },error=>{
+      console.log(error);
     })
   }
 
-  getCurrentUser(){
-    this.accountService.currentUser$.subscribe(user=>{
-      this.loggedIn=!!user;
-    },error=>{
-      console.log(error)
-    });
-  }
   logout(){
     this.accountService.logout();
-    this.loggedIn=false;
   }
 }
